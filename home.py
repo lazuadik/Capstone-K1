e7207918884e0c869ee7b55a97f93d97ef1bb491
paiq <100478607+naufaiqr@users.noreply.github.com>
2024-05-28 15:37:29 +0700

    Update gsheet connection

--- home.py
+++ home.py
@@ -1,16 +1,17 @@
 import streamlit as st
 import pandas as pd
 import sistem_rekomendasi_modul
-from streamlit_gsheets import GSheetsConnection
-
-conn = st.connection("gsheets", type=GSheetsConnection)
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('db-gsheets-422915-23989a936cd5.json', scope)
+client = gspread.authorize(creds)
 
 st.header("Selamat datang di Rekomendasi Karir IT!")
 st.write("Masukkan input, nanti kami akan merekomendasikan jalur karir yang cocok denganmu!")
 
-# Buat store data ke database
-dt = conn.read(usecols=list(range(14)), ttl=5)
-dt = dt.dropna(how="all")
+# Buat store data ke database input
+dt = client.open('Streamlit database').worksheet('input')
 
 with st.form(key='my_form'):
     nama = st.text_input("Masukkan Nama: ")
@@ -30,50 +31,32 @@ with st.form(key='my_form'):
     # ini disini data nama dan data 1 - 10 dimasukin ke database dulu
 
     if submitted:
-           # Mulai dari sini, sistem rekomendasinya berjalan
-       df_new = pd.DataFrame(columns=['Pengelolaan informasi pembelajaran', 'Pengelolaan situasi yang dihadapi', 'Kreativitas dalam bersikap', 'Pola Komunikasi', 'Interaksi dengan lingkungan pembelajaran', 'Pengambilan Keputusan dan Kepemimpinan', 'Minat Karir dan Kesesuaian potensi bidang pekerjaan', 'Saran Pengembangan Diri 1', 'Saran Pengembangan Diri 2', 'Saran Pengembangan Diri 3', 'Saran Pengembangan Diri 4'])
-       item_df = pd.read_csv('training_data_cleaned_translated.csv')
+        # Mulai dari sini, sistem rekomendasinya berjalan
+        df_new = pd.DataFrame(columns=['Pengelolaan informasi pembelajaran', 'Pengelolaan situasi yang dihadapi', 'Kreativitas dalam bersikap', 'Pola Komunikasi', 'Interaksi dengan lingkungan pembelajaran', 'Pengambilan Keputusan dan Kepemimpinan', 'Minat Karir dan Kesesuaian potensi bidang pekerjaan', 'Saran Pengembangan Diri 1', 'Saran Pengembangan Diri 2', 'Saran Pengembangan Diri 3', 'Saran Pengembangan Diri 4'])
 
-       df_new.loc[len(df_new)] = [data1, data2, data3, data4, 
+        item_df = pd.read_csv('training_data_cleaned_translated.csv')
+
+        df_new.loc[len(df_new)] = [data1, data2, data3, data4, 
                                 data5, data6, data7, data8,
                                 data9, data10, data11]
 
-       df_cleaned_new = sistem_rekomendasi_modul.get_user_input(df_new)
-       item_ranking_new, item_ranking_aggregated, item_df  = sistem_rekomendasi_modul.tfidf_new(df_cleaned_new, item_df)
-       top_3_new, top_3_aggregated = sistem_rekomendasi_modul.result(item_ranking_new, item_ranking_aggregated, item_df)
+        df_cleaned_new = sistem_rekomendasi_modul.get_user_input(df_new)
+        item_ranking_new, item_ranking_aggregated, item_df  = sistem_rekomendasi_modul.tfidf_new(df_cleaned_new, item_df)
+        top_3_new, top_3_aggregated = sistem_rekomendasi_modul.result(item_ranking_new, item_ranking_aggregated, item_df)
 
-       # Membuat dataframe dengan index 1, 2, 3 untuk rekomendasi
+        # Membuat dataframe dengan index 1, 2, 3 untuk rekomendasi
+        top_3_df_new = top_3_new
+        top_3_df_aggregated = top_3_aggregated.set_index('Urutan')
 
-       top_3_df_new = top_3_new
-       top_3_df_aggregated = top_3_aggregated.set_index('Urutan')
+        # Menampilkan DataFrame tanpa indeks default menggunakan Streamlit
+        st.subheader("\nRekomendasi top berdasarkan agregasi skor kesamaan pengguna lain:")
+        st.write(top_3_df_aggregated)
 
-# Menampilkan DataFrame tanpa indeks default menggunakan Streamlit
-       
+        top_3_new_joined = ', '.join(top_3_new)
+        top_3_aggregated_joined = ', '.join(top_3_aggregated)
 
-       st.subheader("\nRekomendasi top berdasarkan agregasi skor kesamaan pengguna lain:")
-       st.table(top_3_df_aggregated)
-
-       top_3_new_joined = ', '.join(top_3_new)
-       top_3_aggregated_joined = ', '.join(top_3_aggregated)
-
-       input_dt = pd.DataFrame([{
-           "nama": nama,
-           "pengolalaanInformasiPemebelajaran": data1,
-           "pengelolaanSituasiYgDihadapi": data2,
-           "kreativitasDlmBersikap": data3,
-           "polaKomunikasi": data4,
-           "interaksiDenganLingkunganPembelajaran": data5,
-           "pengambilanKeputusanDanKepemimpinan": data6,
-           "minatKarirDanKesesuaianPotensiBidangPekerjaan": data7,
-           "saranPengembanganDiri_1": data8,
-           "saranPengembanganDiri_2": data9,
-           "saranPengembanganDiri_3": data10,
-           "saranPengembanganDiri_4": data11,
-           "top3new": top_3_new_joined,
-           "top3aggregated": top_3_aggregated_joined
-       }])
+        row_dt = [nama,data1,data2,data3,data4,data5,data6,data7,data8,data9,data10,data11,top_3_new_joined,top_3_aggregated_joined]
         
-       # Update database
-       update_dt = pd.concat([dt, input_dt], ignore_index=True)
-       conn.update(data=update_dt)
-       st.success("Terima kasih sudah berpartisipasi! Semoga anda senang dengan hasil rekomendasinya.") 
+        # Update database
+        dt.append_row(row_dt)
+        st.success("Terima kasih sudah berpartisipasi! Semoga anda senang dengan hasil rekomendasinya.")
